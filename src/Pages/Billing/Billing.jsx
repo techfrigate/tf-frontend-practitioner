90f6fc43cdd06a27f7436faf5b8ba65be0d2c049
Dhruv-Swami <dhruvswaminhr@gmail.com>
2025-04-16 09:10:38 +0530

    billing

--- src/Pages/Billing/Billing.jsx
+++ src/Pages/Billing/Billing.jsx
@@ -6,40 +6,43 @@ import CustomTable from "../../Components/Common/CustomTable";
 import { clearBillingError, getAllBillings } from "../../Store/billingSlice";
 import { fetchLocationById } from "../../Store/locationSlice";
 import { useDispatch, useSelector } from "react-redux";
-import { FileText } from "lucide-react";
 import Loader from "../../Components/Common/Loader";
 import toast from "react-hot-toast";
+import { FileText } from "lucide-react";
 
 const Billing = () => {
   const [currentPage, setCurrentPage] = useState(1);
   const [locationId, setLocationId] = useState("");
-  const [sortConfig, setSortConfig] = useState({sortBy: "updatedAt",order: "desc" });
-  const itemsPerPage = 5;
+  const [sortConfig, setSortConfig] = useState({
+    sortBy: "updatedAt",
+    order: "desc",
+  });
   const dispatch = useDispatch();
   const { totalPages, billings, isLoading, error } = useSelector(
     (state) => state.billing
   );
-  
-  const {profileData} = useSelector((state)=>state.profile);
+
+  const { profileData } = useSelector((state) => state.profile);
 
   useEffect(() => {
     if (profileData) {
       setLocationId(profileData.locations[0]);
-      dispatch(fetchLocationById({locationsId:profileData.locations})); 
-    }
+      console.log(profileData.locations[0]);
+      dispatch(fetchLocationById({ locationsId: profileData.locations }));
+    }// eslint-disable-next-line
   }, [profileData]);
- 
+  console.log(locationId);
   useEffect(() => {
     if (locationId) {
       fetchBillings();
-    }
+    }// eslint-disable-next-line
   }, [locationId, dispatch, currentPage, sortConfig]);
 
   const fetchBillings = () => {
     dispatch(
       getAllBillings({
         currentPage: currentPage,
-        itemsPerPage: 10,
+        itemsPerPage: 5,
         sortBy: sortConfig.sortBy,
         order: sortConfig.order,
         locationId,
@@ -48,13 +51,18 @@ const Billing = () => {
   };
 
   const handleSort = (key) => {
-    if (key === 'updatedAt') {
-      const newOrder = sortConfig.sortBy === 'updatedAt' && sortConfig.order === 'desc' ? 'asc' : 'desc';
-      setSortConfig({ sortBy: 'updatedAt', order: newOrder });
-    } 
-    else if (key === 'status') {
-      const newOrder = sortConfig.sortBy === 'status' && sortConfig.order === 'asc' ? 'desc' : 'asc';
-      setSortConfig({ sortBy: 'status', order: newOrder });
+    if (key === "updatedAt") {
+      const newOrder =
+        sortConfig.sortBy === "updatedAt" && sortConfig.order === "desc"
+          ? "asc"
+          : "desc";
+      setSortConfig({ sortBy: "updatedAt", order: newOrder });
+    } else if (key === "status") {
+      const newOrder =
+        sortConfig.sortBy === "status" && sortConfig.order === "asc"
+          ? "desc"
+          : "asc";
+      setSortConfig({ sortBy: "status", order: newOrder });
     }
   };
 
@@ -68,19 +76,19 @@ const Billing = () => {
       setTimeout(() => {
         dispatch(clearBillingError());
       }, 2000);
-    }
+    }// eslint-disable-next-line
   }, [error]);
 
   return (
-    <div className="overflow-y-auto h-full sm:rounded-lg">
+    <div className="overflow-y-auto h-full w-full sm:rounded-lg">
       {isLoading ? (
         <Loader />
-      ) : billings.length > 0 ? (
+      ) : (
         <>
-          <CustomTable 
+          <CustomTable
             trHeader={
-              <BillingtrHeader 
-                locationId={locationId} 
+              <BillingtrHeader
+                locationId={locationId}
                 setLocationId={setLocationId}
                 handleSort={handleSort}
                 currentSort={sortConfig}
@@ -89,42 +97,45 @@ const Billing = () => {
           >
             <BillingTd />
           </CustomTable>
-          <ReactPaginate
-            previousLabel={"«"}
-            nextLabel={"»"}
-            breakLabel={"..."}
-            pageCount={totalPages}
-            onPageChange={handlePageClick}
-            containerClassName={"flex justify-center mt-6 mb-0"}
-            pageClassName={"mx-1"}
-            pageLinkClassName={
-              "block px-4 py-2 rounded hover:bg-[#4cb59c] hover:text-white"
-            }
-            previousClassName={"mx-1"}
-            previousLinkClassName={
-              "block px-3 py-2 rounded bg-slate-100 hover:bg-[#4cb59c] hover:text-white"
-            }
-            nextClassName={"mx-1"}
-            nextLinkClassName={
-              "block px-3 py-2 rounded bg-slate-100 hover:bg-[#4cb59c] hover:text-white"
-            }
-            activeClassName={"bg-[#64c6b0] text-white rounded"}
-            activeLinkClassName={"border-none"}
-          />
+          {billings.length == 0 && (
+            <div className="flex items-center justify-center min-h-[60vh] w-full">
+              <div className="flex flex-col items-center justify-center bg-gray-50 shadow-md rounded-lg p-6">
+                <FileText className="w-16 h-16 text-[#64c6b0]" />
+                <p className="mt-4 text-lg font-semibold text-gray-600">
+                  No Billing Records Found
+                </p>
+                <p className="text-sm text-gray-500">
+                  No invoices or bills available. Please generate a new billing
+                  record.
+                </p>
+              </div>
+            </div>
+          )}
+          {billings.length > 0 && (
+            <ReactPaginate
+              previousLabel={"«"}
+              nextLabel={"»"}
+              breakLabel={"..."}
+              pageCount={totalPages}
+              onPageChange={handlePageClick}
+              containerClassName={"flex justify-center mt-6 mb-0"}
+              pageClassName={"mx-1"}
+              pageLinkClassName={
+                "block px-4 py-2 rounded hover:bg-[#4cb59c] hover:text-white"
+              }
+              previousClassName={"mx-1"}
+              previousLinkClassName={
+                "block px-3 py-2 rounded bg-slate-100 hover:bg-[#4cb59c] hover:text-white"
+              }
+              nextClassName={"mx-1"}
+              nextLinkClassName={
+                "block px-3 py-2 rounded bg-slate-100 hover:bg-[#4cb59c] hover:text-white"
+              }
+              activeClassName={"bg-[#64c6b0] text-white rounded"}
+              activeLinkClassName={"border-none"}
+            />
+          )}
         </>
-      ) : (
-        <div className="flex items-center justify-center w-full h-full">
-          <div className="flex flex-col items-center justify-center bg-gray-50 shadow-md rounded-lg p-6">
-            <FileText className="w-16 h-16 text-[#64c6b0]" />
-            <p className="mt-4 text-lg font-semibold text-gray-600">
-              No Billing Records Found
-            </p>
-            <p className="text-sm text-gray-500">
-              No invoices or bills available. Please generate a new billing
-              record.
-            </p>
-          </div>
-        </div>
       )}
     </div>
   );
--- src/Pages/Billing/BillingTd.jsx
+++ src/Pages/Billing/BillingTd.jsx
@@ -2,10 +2,9 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 
-
 const BillingTd = () => {
   const navigate = useNavigate();
-  const {billings } = useSelector((state) => state.billing);
+  const { billings } = useSelector((state) => state.billing);
   const navigateToBill = (id) => {
     navigate(`/add-bill?id=${id}`);
   };
@@ -15,30 +14,24 @@ const BillingTd = () => {
   };
   return (
     <>
-      {billings?.map((item) => (
+      {billings.map((item) => (
         <tr
           key={item._id}
           onClick={() => navigateToBill(item._id)}
-          className="hover:bg-gray-100 bg-gray-50 border border-gray-300 hover:shadow-lg transition duration-300 ease-in-out cursor-pointer "
+          className="hover:bg-gray-100 bg-gray-50 border border-gray-300 hover:shadow-lg transition duration-300 ease-in-out cursor-pointer"
         >
-         
-          <td className="py-3 w-[15%] px-6 font-medium ">
+          <td className="py-3 w-[15%] px-6 font-medium">
             {item.patientName}
-    
             <div className="text-xs text-gray-600 mt-1">
               {item.phoneNumber.dialCode} {item.phoneNumber.value}
             </div>
           </td>
-          <td className="py-4 px-6 text-left">
-          {item.locationDisplayName}
-          </td>
-          <td className="py-4 px-6 ">
-            {item.uhid}
-          </td>
+          <td className="py-4 px-6 text-left">{item.locationDisplayName}</td>
+          <td className="py-4 px-6">{item.uhid}</td>
           <td className="py-4 px-6">{item.billId}</td>
           <td className="py-4 px-6">
             <span
-              className={`inline-flex items-center rounded-md border px-2.5  py-0.5 text-xs font-semibold ${
+              className={`inline-flex items-center rounded-md border px-2.5 py-0.5 text-xs font-semibold ${
                 item.status === false
                   ? "bg-blue-100 text-blue-900"
                   : "bg-green-100 text-green-900"
